77653d7682b782ea89f2de29bcc358b71a1fb201
Neil <nahidnoriel@gmail.com>
2019-02-12 18:07:24 +0800

    Added Icons on user and password
    Added a 128bit png icon for username and password fields

--- CJLS_FOODS_PAYROLL_SYSTEM/CJLS_FOODS_PAYROLL_SYSTEM/Views/Form_Login.xaml.cs
+++ CJLS_FOODS_PAYROLL_SYSTEM/CJLS_FOODS_PAYROLL_SYSTEM/Views/Form_Login.xaml.cs
@@ -10,6 +10,7 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Media.Animation;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using CJLS_FOODS_PAYROLL_SYSTEM.Models;
@@ -23,28 +24,49 @@ namespace CJLS_FOODS_PAYROLL_SYSTEM {
         DatabaseDataContext db = new DatabaseDataContext();
         public MainWindow() {
             InitializeComponent();
+            DoubleAnimation da = new DoubleAnimation(0, 1, new Duration(TimeSpan.FromSeconds(1)));
+            BeginAnimation(Window.OpacityProperty, da);
         }
 
-        private void Lbl_forgetPassword_MouseEnter(object sender, MouseEventArgs e) {
-            lbl_forgetPassword.Foreground = new SolidColorBrush(Colors.Violet);
-        }
-        private void Lbl_forgetPassword_MouseLeave(object sender, MouseEventArgs e) {
-            lbl_forgetPassword.Foreground = new SolidColorBrush(Colors.Black);
+        private void PressedEnterKey(object sender, KeyEventArgs e) {
+            if (e.Key == Key.Enter)
+                btn_login_Click(sender, e);
         }
 
-        private void Btn_login_Click(object sender, RoutedEventArgs e) {
-            if(txtbox_password.Text == "" && txtbox_userName.Text == "") {
+        private void textBox_GotFocus(object sender, RoutedEventArgs e) {
+            var txtbox = (TextBox)sender;
+            txtbox.Foreground = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#171A21"));
+            if (txtbox.Text == "Enter username" || txtbox.Text == "Enter password") {
+                txtbox.Text = "";
+            }
+            else {
+                txtbox.SelectAll();
+            }
+        }
+        private void textBox_LeaveFocus(object sender, RoutedEventArgs e) {
+            var txtbox = (TextBox)sender;
+            if (txtbox.Name == "txtbox_username" && txtbox.Text == "") {
+                txtbox.Foreground = new SolidColorBrush(Colors.Gray);
+                txtbox.Text = "Enter username";
+            }
+            else if (txtbox.Name == "txtbox_password" && txtbox.Text == "") {
+                txtbox.Foreground = new SolidColorBrush(Colors.Gray);
+                txtbox.Text = "Enter password";
+            }
+        }
+        private void btn_login_Click(object sender, RoutedEventArgs e) {
+            if (txtbox_password.Text == "" && txtbox_username.Text == "") {
                 MessageBox.Show("You must provide username and password.");
             }
-            else if(txtbox_userName.Text == "") {
+            else if (txtbox_username.Text == "") {
                 MessageBox.Show("You must provide a username.");
             }
-            else if(txtbox_password.Text == "") {
+            else if (txtbox_password.Text == "") {
                 MessageBox.Show("You must provide a password");
             }
             else {
-                var result = db.sp_checkLogin(txtbox_userName.Text, txtbox_password.Text).ToList();
-                if(result.Count > 0) {
+                var result = db.sp_checkLogin(txtbox_username.Text, txtbox_password.Text).ToList();
+                if (result.Count > 0) {
                     //successful login
                     MessageBox.Show("Successfully logged in!");
                     Form_Dashboard form_Dashboard = new Form_Dashboard();
@@ -58,14 +80,11 @@ namespace CJLS_FOODS_PAYROLL_SYSTEM {
                 else {
                     //incorrect credentials
                     MessageBox.Show("Incorrect Credentials, try again.");
-                    txtbox_password.Text = "";
-                    txtbox_userName.Text = "";
+                    txtbox_password.Text = "Enter password";
+                    txtbox_username.Text = "Enter username";
                 }
             }
         }
 
-        private void Window_RequestBringIntoView(object sender, RequestBringIntoViewEventArgs e) {
-
-        }
     }
 }
