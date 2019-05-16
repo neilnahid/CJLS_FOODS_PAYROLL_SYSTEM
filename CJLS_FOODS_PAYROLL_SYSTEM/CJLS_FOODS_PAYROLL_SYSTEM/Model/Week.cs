0c159060b747b29a538f0123a2d426ba2594de9f
Neil <nahidnoriel@gmail.com>
2019-05-17 00:04:56 +0800

    started working on Attendance UI

---
+++ CJLS_FOODS_PAYROLL_SYSTEM/CJLS_FOODS_PAYROLL_SYSTEM/Model/Week.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CJLS_FOODS_PAYROLL_SYSTEM.Model {
+    public class Week : ModelPropertyChange {
+        public Week() {
+            Days = new List<Day>();
+            for(int i = 0; i < 7; i++) {
+                Days.Add(new Day());
+            }
+        }
+        private List<Day> days;
+
+        public List<Day> Days {
+            get { return days; }
+            set {
+                if (days != value) {
+                    days = value;
+                    RaisePropertyChanged("Days");
+                }
+            }
+        }
+    }
+}
---
+++ CJLS_FOODS_PAYROLL_SYSTEM/CJLS_FOODS_PAYROLL_SYSTEM/View Models/AttendanceViewModel.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CJLS_FOODS_PAYROLL_SYSTEM.View_Models {
+    public class AttendanceViewModel : Model.ModelPropertyChange {
+
+        public AttendanceViewModel() {
+            startDate = new DateTime(2019, 5, 1);
+            endDate = startDate.AddMonths(1);
+            Month = GetMonthRange();
+        }
+        private DateTime startDate;
+
+        public DateTime StartDate {
+            get { return startDate; }
+            set {
+                if (startDate != value) {
+                    startDate = value;
+                    RaisePropertyChanged("StartDate");
+                }
+            }
+        }
+        private DateTime endDate;
+
+        public DateTime EndDate {
+            get { return endDate; }
+            set {
+                if (endDate != value) {
+                    endDate = value;
+                    RaisePropertyChanged("EndDate");
+                }
+            }
+        }
+        private Model.Month month;
+
+        public Model.Month Month {
+            get { return month; }
+            set {
+                if (month != value) {
+                    month = value;
+                    RaisePropertyChanged("Month");
+                }
+            }
+        }
+        public Model.Month GetMonthRange() {
+            var month = new Model.Month();
+            month.Weeks = new List<Model.Week>();
+            month.Weeks.Add(new Model.Week());
+            int weekCounter = 0;
+            for (int j = 0; j < (EndDate - StartDate).TotalDays; j++) {
+                if (startDate.AddDays(j).DayOfWeek == DayOfWeek.Saturday) {
+                    var currentDay = month.Weeks[weekCounter].Days[(int)startDate.AddDays(j).DayOfWeek];
+                    currentDay.DateTime = startDate.AddDays(j);
+                    currentDay.MonthNumber = startDate.AddDays(j).Day;
+                    month.Weeks.Add(new Model.Week());
+                    weekCounter++;
+                }
+                else {
+                    var currentDay = month.Weeks[weekCounter].Days[(int)startDate.AddDays(j).DayOfWeek];
+                    currentDay.DateTime = startDate.AddDays(j);
+                    currentDay.MonthNumber = startDate.AddDays(j).Day;
+                }
+            }
+            return month;
+        }
+    }
+}
