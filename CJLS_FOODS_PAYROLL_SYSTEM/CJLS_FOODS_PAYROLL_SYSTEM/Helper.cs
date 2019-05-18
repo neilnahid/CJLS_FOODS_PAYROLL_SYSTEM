3ad8de945c3d0bd0673a39d78e0634fa7062721b
Neil <nahidnoriel@gmail.com>
2019-05-19 05:52:18 +0800

    made some changes.

--- CJLS_FOODS_PAYROLL_SYSTEM/CJLS_FOODS_PAYROLL_SYSTEM/Helper.cs
+++ CJLS_FOODS_PAYROLL_SYSTEM/CJLS_FOODS_PAYROLL_SYSTEM/Helper.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using AutoMapper;
 namespace CJLS_FOODS_PAYROLL_SYSTEM {
     public static class Helper {
         public static DatabaseDataContext db = new DatabaseDataContext();
