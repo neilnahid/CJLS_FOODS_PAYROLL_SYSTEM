8bd07069ff4cad2793371fb15b1ec72129cbc083
neilnahid1 <neil.nahid@fullspeedtechnologies.com>
2019-09-22 04:51:03 +0800

    added validation for employee

--- CJLS_FOODS_PAYROLL_SYSTEM/CJLS_FOODS_PAYROLL_SYSTEM/Model/Employee.cs
+++ CJLS_FOODS_PAYROLL_SYSTEM/CJLS_FOODS_PAYROLL_SYSTEM/Model/Employee.cs
@@ -20,12 +20,51 @@ namespace CJLS_FOODS_PAYROLL_SYSTEM
                             goto case "stringEmpty";
                         break;
                     case "MiddleName":
+                        if (String.IsNullOrEmpty(MiddleName))
+                            goto case "stringEmpty";
+                        break;
+                    case "LastName":
                         if (String.IsNullOrEmpty(LastName))
                             goto case "stringEmpty";
                         break;
+                    case "Gender":
+                        if (String.IsNullOrEmpty(Gender))
+                            goto case "stringEmpty";
+                        break;
+                    case "Address":
+                        if (String.IsNullOrEmpty(Address))
+                            goto case "stringEmpty";
+                        break;
+                    case "EmployeeType":
+                        if (EmployeeType == null)
+                            result = "You must select a job position.";
+                        break;
+                    case "ContactNumber":
+                        if (String.IsNullOrEmpty(ContactNumber))
+                            goto case "stringEmpty";
+                        break;
+                    case "HourlyRate":
+                        if (String.IsNullOrEmpty(HourlyRate.ToString()))
+                            goto case "stringEmpty";
+                        break;
+                    case "Branch":
+                        if (String.IsNullOrEmpty(Branch))
+                            goto case "stringEmpty";
+                        break;
+                    case "PayrollGroup":
+                        if (PayrollGroup == null)
+                            result = "You must select a Payroll Group";
+                        break;
+                    case "Status":
+                        if (String.IsNullOrEmpty(Status))
+                            goto case "stringEmpty";
+                        break;
+                    case "RequiredDaysAWeek":
+                        if (Double.IsNaN(RequiredDaysAWeek))
+                            result = "you must specify number of days required per week.";
+                        break;
+
                     case "stringEmpty": result = "Field must not be empty."; break;
-                    default:
-                        result = "invalid property error"; break;
                 }
                 if (ErrorCollection.ContainsKey(name))
                     ErrorCollection[name] = result;
