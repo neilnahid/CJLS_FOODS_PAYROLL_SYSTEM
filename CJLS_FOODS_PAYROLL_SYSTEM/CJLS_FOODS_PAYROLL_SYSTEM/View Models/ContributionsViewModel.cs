689c259d911833c46225f4478b17836e832e1e6d
Neil Francis Nahid <nahidnoriel@gmail.com>
2019-10-09 02:52:50 +0800

    added contribution total for the whole year.

--- CJLS_FOODS_PAYROLL_SYSTEM/CJLS_FOODS_PAYROLL_SYSTEM/View Models/ContributionsViewModel.cs
+++ CJLS_FOODS_PAYROLL_SYSTEM/CJLS_FOODS_PAYROLL_SYSTEM/View Models/ContributionsViewModel.cs
@@ -13,6 +13,48 @@ namespace CJLS_FOODS_PAYROLL_SYSTEM.View_Models
 
         public ObservableCollection<Employee> Employees { get; set; }
         public ObservableCollection<Employee> FilteredEmployees { get; set; }
+        public double? TotalIncomeTax {
+            get {
+                if (Employee != null)
+                {
+                    var total = Employee.PayrollDetails.Where(pd=>pd.Payroll.StartDate.Year == DateTime.Now.Year).Sum(pd => pd.TaxTotal);
+                    return total.HasValue ? total : 0;
+                }
+                return 0;
+            }
+        }
+
+        public double? TotalPagibig {
+            get {
+                if (Employee != null)
+                {
+                    var total = Employee.PayrollDetails.Where(pd => pd.Payroll.StartDate.Year == DateTime.Now.Year).Sum(pd => pd.PagibigTotal);
+                    return total.HasValue ? total : 0;
+                }
+                return 0;
+            }
+        }
+        public double? TotalSSS {
+            get {
+                if (Employee != null)
+                {
+                    var total = Employee.PayrollDetails.Where(pd => pd.Payroll.StartDate.Year == DateTime.Now.Year).Sum(pd => pd.SSSTotal);
+                    return total.HasValue ? total : 0;
+                }
+                return 0;
+            }
+        }
+        public double? TotalPhilHealth {
+            get {
+                if (Employee != null)
+                {
+                    var total = Employee.PayrollDetails.Where(pd => pd.Payroll.StartDate.Year == DateTime.Now.Year).Sum(pd => pd.PhilhealthTotal);
+                    return total.HasValue ? total : 0;
+                }
+                return 0;
+            }
+        }
+        [PropertyChanged.AlsoNotifyFor("TotalIncomeTax", "TotalPagibig", "TotalSSS", "TotalPhilHealth")]
         public Employee Employee { get; set; }
         public event PropertyChangedEventHandler PropertyChanged;
         public int Page { get; set; }
