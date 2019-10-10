d01bbe9b564d13c2bfc6ad1b892fc39e1d8f8188
Neil Francis Nahid <nahidnoriel@gmail.com>
2019-10-10 21:42:14 +0800

    added running balance feature in contribtions form

--- CJLS_FOODS_PAYROLL_SYSTEM/CJLS_FOODS_PAYROLL_SYSTEM/View Models/ContributionsViewModel.cs
+++ CJLS_FOODS_PAYROLL_SYSTEM/CJLS_FOODS_PAYROLL_SYSTEM/View Models/ContributionsViewModel.cs
@@ -17,7 +17,7 @@ namespace CJLS_FOODS_PAYROLL_SYSTEM.View_Models
             get {
                 if (Employee != null)
                 {
-                    var total = Employee.PayrollDetails.Where(pd=>pd.Payroll.StartDate.Year == DateTime.Now.Year).Sum(pd => pd.TaxTotal);
+                    var total = Employee.PayrollDetails.Where(pd => pd.Payroll.StartDate.Year == DateTime.Now.Year).Sum(pd => pd.TaxTotal);
                     return total.HasValue ? total : 0;
                 }
                 return 0;
@@ -53,6 +53,88 @@ namespace CJLS_FOODS_PAYROLL_SYSTEM.View_Models
                 }
                 return 0;
             }
+        }
+        public double? TotalCurrentContributions {
+            get {
+                if (Employee != null)
+                {
+                    var total = TotalPhilHealth + TotalSSS + TotalPagibig + TotalIncomeTax;
+                    return total.HasValue ? total : 0;
+                }
+                return 0;
+            }
+        }
+        public double? RunningBalance {
+            get {
+                return YearlyContribution.Value - TotalCurrentContributions.Value;
+            }
+        }
+        public double? YearlyContribution {
+            get {
+                if (Employee != null)
+                {
+                    double philhealthyearly = ((Employee.MonthlySalary.Value < 10000 ? 10000 : Employee.MonthlySalary.Value > 40000 ? 40000 : Employee.MonthlySalary.Value) * .01375) * 12;
+                    var tax = getAnnualTax();
+                    var sss = getAnnualSSS();
+                    var pagibig = (Employee.MonthlySalary < 1500 ? 1500 * 0.01 : Employee.MonthlySalary > 5000 ? 5000 * 0.02 : Employee.MonthlySalary * 0.02) * 12;
+                    return Math.Round((philhealthyearly + tax + sss + pagibig).Value,2);
+                }
+                return 0;
+            }
+        }
+        private double getAnnualSSS()
+        {
+            double msc = 0.0;
+            if (Employee.MonthlySalary < 2250)
+                msc = 2000;
+            else if (Employee.MonthlySalary >= 19750)
+                msc = 20000;
+            else
+                msc = Math.Round(Employee.MonthlySalary.Value / 500, 0) * 500;
+
+            return Math.Round(msc * .04, 2) * 12;
+        }
+        private double getAnnualTax()
+        {
+            double rate = 0;
+            double clAmount = 0;
+            double total = 0;
+            if (Employee.MonthlySalary <= 20833)
+                rate = 0;
+            else if (Employee.MonthlySalary > 20833 && Employee.MonthlySalary < 33333)
+            {
+                rate = 0.2;
+                clAmount = 20833;
+            }
+            else if (Employee.MonthlySalary >= 33333 && Employee.MonthlySalary < 66667)
+            {
+                total = 2500;
+                rate = 0.25;
+                clAmount = 33333;
+
+            }
+            else if (Employee.MonthlySalary > 66667 && Employee.MonthlySalary < 166667)
+            {
+                total = 10833.33;
+                rate = 0.30;
+                clAmount = 66667;
+
+            }
+            else if (Employee.MonthlySalary > 166667 && Employee.MonthlySalary < 666667)
+            {
+                total = 40833.33;
+                rate = 0.32;
+                clAmount = 166667;
+
+            }
+            else if (Employee.MonthlySalary >= 666667)
+            {
+                total = 200833.33;
+                rate = 0.35;
+                clAmount = 20833;
+
+            }
+            return ((Employee.MonthlySalary.Value - clAmount) * rate + total) * 12;
         }
         [PropertyChanged.AlsoNotifyFor("TotalIncomeTax", "TotalPagibig", "TotalSSS", "TotalPhilHealth")]
         public Employee Employee { get; set; }
